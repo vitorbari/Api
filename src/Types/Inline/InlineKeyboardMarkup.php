a5c202d7e791a2af19f3e9822d435b8c720caa5f
Vitor Bari Buccianti <vitorbari@gmail.com>
2017-03-06 10:20:20 -0300

    Fix inline keyboard markup

--- src/Types/Inline/InlineKeyboardMarkup.php
+++ src/Types/Inline/InlineKeyboardMarkup.php
@@ -10,5 +10,21 @@ namespace TelegramBot\Api\Types\Inline;
 
 class InlineKeyboardMarkup
 {
+    /**
+     * Array of button rows, each represented by an Array of Strings
+     * Array of Array of String
+     *
+     * @var array
+     */
+    public $inline_keyboard;
 
+    public function __construct($inline_keyboard)
+    {
+        $this->inline_keyboard = $inline_keyboard;
+    }
+
+    public function toJson($inner = false)
+    {
+        return json_encode($this);
+    }
 }
